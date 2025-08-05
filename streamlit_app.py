ff67ccd6774f70e0ae13c314cbf071543afda775
brianfachini-sesisenai <brian_fachini@estudante.sesisenai.org.br>
2025-08-05 08:48:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ def reservas_extras():
 # -------- FUNÇÃO DE PAGAMENTO --------
 def pagamento():
     st.header("💳 Pagamento da Hospedagem")
-    preco_hospedagem = st.number_input("Valor da Hospedagem (R$)", min_value=0.0, format="%.2f")
+    preco_total = st.write("Valor da Hospedagem (R$)", min_value=0.0, format="%.2f")
     nome = st.text_input("Nome no cartão")
     numero = st.text_input("Número do cartão")
     validade = st.text_input("Validade (MM/AA)")
@@ -107,3 +107,4 @@ else:
         pagamento()
     elif opcao == "FAQ":
         faq()
+
